223928b06c6bbaaf06867acb14130bee36b0d0e1
SoeSandarWin472 <soesandarwin472000@gmail.com>
2023-07-31 13:55:47 +0630

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>HI</h1>
+  <Modal></Modal>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Modal from "./components/Modal.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      title:"u know js"
+
+    }
+  },
+ 
+  components:{
+    Modal
   }
+
 }
 </script>
-
+ 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1{
+  color: blue;
 }
 </style>
